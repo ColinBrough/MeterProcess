07632eb03df4119055fab5a6079fd4f95878eff1
Colin Brough <colinbrough73@gmail.com>
2023-02-01 15:01:33 +0000

    Changes to check more features of cmbDate.java class

--- src/MeterProcessMain.java
+++ src/MeterProcessMain.java
@@ -16,12 +16,19 @@ public class MeterProcessMain
 
     public static void main(String[] args)
     {
-        cmbDate date;
-        date = new cmbDate(2023, 1, 30);
-        date.incDate(-2);
+        char Grtr[] = { '<', '=', '>' };
+                        
+        cmbDate date1, date2;
+        date1 = new cmbDate(2023, 1, 30);
+        date2 = new cmbDate(2023, 1, 30);
+        date1.incDate(2);
+        date2.incDate(-2);
         
-        System.out.printf("Date is %02d/%02d/%d\n",
-                          date.getDay(), date.getMonth(), date.getYear());
+        System.out.printf("Date1 is %02d/%02d/%d\n",
+                          date1.getDay(), date1.getMonth(), date1.getYear());
+        System.out.printf("Date2 is %02d/%02d/%d\n",
+                          date2.getDay(), date2.getMonth(), date2.getYear());
+        System.out.printf("Date1 is %c than Date2\n", Grtr[date1.compareTo(date2) + 1]);
     }
 
     
